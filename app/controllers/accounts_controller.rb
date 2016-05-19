ae95e5a6807b59ac1d07cba60a8dd8f7f3fcef3a
Franck ARNULFO <franck.arnulfo@gmail.com>
2016-05-19 13:22:43 +0200

    Filtre sur les comptes de l'utilisateurs dans la liste des comptes

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -5,7 +5,8 @@ class AccountsController < ApplicationController
   # GET /accounts
   # GET /accounts.json
   def index
-    @accounts = Account.all
+    # @accounts = Account.all
+    @accounts = Account.find_by_user_id(current_user.id) || []
   end
 
   # GET /accounts/1
